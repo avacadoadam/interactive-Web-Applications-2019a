37bd3abaca8b592d09026be35dbaa8bc228e00a0
Adam Sever <avacadoadam@gmail.com>
2019-12-02 21:26:06 +0000

    updated the readDatabase function to include ES6 default param feature.

--- blog.js
+++ blog.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 
 
+
 /**
  * Reads a content from a json file where the database data resides.
  * The path given is a relative path to this file.
@@ -8,11 +9,10 @@ const fs = require('fs');
  * RETURNS the content of the file or a err
  * If insufficient permission and or file does not exits.
  */
-function readDatabase(databaseFileName) {
-    let fileName = databaseFileName || 'blogDatabase.json';
-    fs.readFile(fileName, 'utf8', function (err, contents) {
+function readDatabase(databaseFileName = 'blogDatabase.json') {
+    fs.readFile(databaseFileName, 'utf8', function (err, contents) {
         if (err) {
-            console.error("Could not find the " + fileName + " file or permission was not given");
+            console.error("Could not find the " + databaseFileName + " file or permission was not given");
             return err;
         } else {
             return contents;
