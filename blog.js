29d52c8595179bf6e5a1ee0782bbb9a8da50f546
Adam Sever <avacadoadam@gmail.com>
2019-12-05 02:28:01 +0000

    Wrapped the asynchronous function fs.readFile in a promise that error checks, The content of the file being properly JSON formatted, and that the database is not empty.

--- blog.js
+++ blog.js
@@ -1,29 +1,8 @@
 const fs = require('fs');
 express = require('express');
-var http = require('http');
+let http = require('http');
 
-//14 - 20 error code = database failure
-let content = null;
 let database = {};
-try {
-    content = readDatabase();
-} catch (e) {
-    console.error(e);
-    process.exit(14);
-}
-
-if (content || content === '') {
-    console.error('Database is empty');
-    process.exit(15);
-}
-
-try {
-    database = JSON.parse(content);
-} catch (e) {
-    console.log("Database is not json or is corrupted");
-    console.error(e);
-    process.exit(16);
-}
 
 let app = express();
 let server = http.createServer(app);
@@ -31,7 +10,17 @@ let server = http.createServer(app);
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
+content = readDatabase()
+    .then(database => {
+        this.database = database;
+        server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function () {
+            var addr = server.address();
+            console.log("Server listening at", addr.address + ":" + addr.port);
+        });
+    });
+
 app.get('/blog', (req, res) => {
+
 });
 app.post('/blog', (req, res) => {
 });
@@ -40,28 +29,46 @@ app.put('/blog', (req, res) => {
 app.delete('/blog', (req, res) => {
 });
 
-//This is where we as the server to be listening to user with a specified IP and Port
-server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function () {
-    var addr = server.address();
-    console.log("Server listening at", addr.address + ":" + addr.port);
-});
-
 /**
  * Reads a content from a json file where the database data resides.
  * The path given is a relative path to this file.
- * A databaseFile is not need to try and stay true to functional programming.
- * RETURNS the content of the file or a err
- * If insufficient permission and or file does not exits.
+ * The databaseFile para is not need to try and stay true to functional programming.
+ * Will fail if insufficient permission and or file does not exits.
+ * @param databaseFileName optional database file name
+ * @returns {Promise<Object>} returns the parse database json file.
  */
-function readDatabase(databaseFileName = 'blogDatabase.json') {
-    fs.readFile(databaseFileName, 'utf8', function (err, contents) {
-        if (err) {
-            console.error("Could not find the " + databaseFileName + " file or permission was not given");
-            return err;
-        } else {
-            return contents;
-        }
+async function readDatabase(databaseFileName = 'blogDatabase.json') {
+    return new Promise((resolve, reject) => {
+        fs.readFile(databaseFileName, 'utf8', (err, contents) => {
+            if (err) {
+                logAndExit(14, "Could not find the " + databaseFileName + " file or permission was not given", err);
+            } else {
+                console.log(contents);
+                if (!contents || contents === '') {
+                    logAndExit(15, "Database is empty");
+                }
+                let database = null;
+                try {
+                    database = JSON.parse(contents);
+                } catch (e) {
+                    logAndExit(16, "Database is not json or is corrupted", e);
+                }
+                resolve(database);
+            }
+        });
     });
+}
+
+/**
+ * A helper function to display a error message and exit system with status code
+ * @param statusCode Exit code
+ * @param errMsg Error message
+ * @param error optional stack trace
+ */
+function logAndExit(statusCode, errMsg, error = null) {
+    console.error(errMsg);
+    if (error) console.log(error);
+    process.exit(statusCode);
 }
 
 
